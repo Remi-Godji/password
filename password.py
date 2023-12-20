a44c93912893f99007036661e705679564b0d021
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2023-12-20 10:36:56 +0100

    Update password.py

--- password.py
+++ password.py
@@ -1,19 +1,47 @@
-import json
+import random
+import string
 import hashlib
+import json
 
 PASSWORDS_FILE = "passwords.json"
+
+def generate_random_password():
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(8))
+    return password
+
+def is_valid_password(password):
+    if len(password) < 8:
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char.islower() for char in password):
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char in string.punctuation for char in password):
+        return False
+    return True
 
 def encrypt_password(password):
     sha256_hash = hashlib.sha256()
     sha256_hash.update(password.encode('utf-8'))
-    return sha256_hash.hexdigest()
+    encrypted_password = sha256_hash.hexdigest()
+    return encrypted_password
 
 def load_passwords():
     try:
         with open(PASSWORDS_FILE, 'r') as file:
-            return json.load(file)
+            data = file.read()
+            if data:
+                return json.loads(data)
+            else:
+                return {}
     except FileNotFoundError:
         return {}
+    except json.decoder.JSONDecodeError:
+        return {}
+
 
 def save_passwords(passwords):
     with open(PASSWORDS_FILE, 'w') as file:
@@ -21,12 +49,16 @@ def save_passwords(passwords):
 
 def add_password():
     username = input("Entrez le nom d'utilisateur : ")
-    password = input("Entrez le mot de passe : ")
+    user_password = input("Entrez le mot de passe : ")
 
-    passwords = load_passwords()
-    passwords[username] = encrypt_password(password)
-    save_passwords(passwords)
-    print(f"Le mot de passe pour {username} a été ajouté avec succès.")
+    if is_valid_password(user_password):
+        encrypted_password = encrypt_password(user_password)
+        passwords = load_passwords()
+        passwords[username] = encrypted_password
+        save_passwords(passwords)
+        print(f"Le mot de passe pour {username} a été ajouté avec succès.")
+    else:
+        print("Le mot de passe ne respecte pas les exigences de sécurité. Veuillez choisir un nouveau mot de passe.")
 
 def display_passwords():
     passwords = load_passwords()
